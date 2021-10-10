aaa3f58bc0693a86b8c6cbcace381c47a1d08992
rupikz <kydry.nikolau@gmail.com>
2021-10-11 01:43:25 +0300

    fix response dto

--- src/dtos/most-changed-balance.dto.ts
+++ src/dtos/most-changed-balance.dto.ts
@@ -3,10 +3,18 @@ import Big from 'big.js';
 import { MostChangedBalanceInterface } from 'src/interfaces/most-changed-balance.interface';
 
 export class MostChangedBalanceDto {
-  @ApiProperty({ type: 'string' })
+  @ApiProperty({
+    type: 'string',
+    description: 'Адрес счета, который изменился больше всего',
+    example: '0x7be8076f4ea4a4ad08075c2508e481d6c946d12b',
+  })
   readonly address: string;
 
-  @ApiProperty({ type: 'integer' })
+  @ApiProperty({
+    type: 'string',
+    description: 'Сумма на которую изменился счет',
+    example: '7.0028',
+  })
   readonly value: Big;
 
   constructor(data: MostChangedBalanceInterface) {
